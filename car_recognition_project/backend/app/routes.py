fb141092ec460643d3f920ca6b6503ab5dbdb6c5
aoaye <aoayeisaac@gmail.com>
2025-04-01 10:59:26 +0100

    Optimized routes.py for efficiency

--- car_recognition_project/backend/app/routes.py
+++ car_recognition_project/backend/app/routes.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI, UploadFile, File
 import shutil
 from app.predict import predict_car
 from pathlib import Path
+import os
 
 app = FastAPI()
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     file_path = Path(f"temp/{file.filename}")
+    
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    # Delete the file if it already exists
+    if file_path.exists():
+        file_path.unlink()
+  
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
